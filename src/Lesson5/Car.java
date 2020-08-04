d8123fa3fa6b122a16c723ecf710f3fde420de56
Igor B <blinovigo@yandex.ru>
2020-08-04 08:01:40 +0300

    Выполнил задание к уроку 5

--- src/Lesson5/Car.java
+++ src/Lesson5/Car.java
@@ -1,7 +1,12 @@
 package Lesson5;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    CyclicBarrier cbStart;
+    CyclicBarrier cbFinish;
     static {
         CARS_COUNT = 0;
     }
@@ -18,11 +23,13 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier cbStart, CyclicBarrier cbFinish) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cbStart = cbStart;
+        this.cbFinish = cbFinish;
     }
 
     @Override
@@ -31,11 +38,19 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cbStart.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+        try {
+            cbFinish.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/Lesson5/MainClass.java
+++ src/Lesson5/MainClass.java
@@ -1,20 +1,36 @@
 package Lesson5;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 public class MainClass {
 
     public static final int CARS_COUNT = 4;
 
     public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(MainClass.CARS_COUNT/2);
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(semaphore), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
+
+        CyclicBarrier cbStart = new CyclicBarrier(CARS_COUNT, new Runnable() {
+            public void run() {
+                System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            }
+        });
+
+        CyclicBarrier cbFinish = new CyclicBarrier(CARS_COUNT, new Runnable() {
+            public void run() {
+                System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+            }
+        });
+
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cbStart, cbFinish);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
